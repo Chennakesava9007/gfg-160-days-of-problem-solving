8c1b3f916f2cfd632c9312cbe9d4d1ad7e7ce5f5
Chennakesava reddy Siddavatam <chennakesava9007@gmail.com>
2025-08-22 23:59:44 +0530

    Delete Day-01-Second-Largest-Element directory

--- Day-01-Second-Largest-Element/solution_sorting.py
+++
@@ -1,9 +0,0 @@
-def getSecondLargest(arr):
-    n = len(arr)
-    if n < 2:
-        return -1
-    arr.sort()
-    for i in range(n-2, -1, -1):
-        if arr[i] != arr[n-1]:
-            return arr[i]
-    return -1
